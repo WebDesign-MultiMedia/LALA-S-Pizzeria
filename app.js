cac1986c85e341a4fe64c390ebc1a056fd337e7b
WebDesign-MultiMedia <110946138+WebDesign-MultiMedia@users.noreply.github.com>
2025-06-09 16:44:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 let mobileNav = document.getElementById('navLinks');
 let barIcon = document.getElementById('fa-bar');
-let comeEatMsg = document.getElementById('msg')
 mobileNav.style.display = "none";
 barIcon.addEventListener("click", ()=>{
     if(mobileNav.style.display === 'block'){
             mobileNav.style.display = 'none';
     }else{
             mobileNav.style.display = 'block';
-            comeEatMsg.style.display = 'none';
     }
 })
